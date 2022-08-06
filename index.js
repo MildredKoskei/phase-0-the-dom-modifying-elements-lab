731bc45a37a3b02859ce0289eaec8bdd5d200ca7
Mildred Chepkoech Koskei <mildredkoskei01@gmail.com>
2022-08-05 23:00:21 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector('main').remove('main');
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "MILDRED is the champion"; 
+
+
